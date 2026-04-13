55c3da3b629cc286d9dd6d9e61954d0ad9ba48c4
Michael Abayomi-Ojumu <abayojumumichael@gmail.com>
2026-04-13 16:05:24 +0100

    Add blocking stack interface

---
+++ src/BlockingStack.h
@@ -0,0 +1,64 @@
+/*
+ * BlockingStack.h
+ *
+ * Module interface for a generic fixed-size Blocking Stack implementation.
+ *
+ */
+
+#ifndef BLOCKING_STACK_H_
+#define BLOCKING_STACK_H_
+
+#include <stdbool.h>
+#include <pthread.h>
+#include "Stack.h"
+
+typedef struct BlockingStack BlockingStack;
+
+struct BlockingStack {
+	Stack* stack;
+	pthread_mutex_t mutex;
+	pthread_cond_t not_empty;
+	pthread_cond_t not_full;
+};
+
+/*
+ * Creates a new BlockingStack for at most max_size void* elements.
+ * Returns a pointer to valid BlockingStack* on success and NULL on failure.
+ */
+BlockingStack* new_BlockingStack(int max_size);
+
+/*
+ * Pushes the given void* element onto this Stack.
+ * If the stack is full, the function will block the calling thread until there is space in the stack.
+ * Returns false when element is NULL and true on success.
+ */
+bool BlockingStack_push(BlockingStack* this, void* element);
+
+/*
+ * Pops the top element from this Stack.
+ * If the stack is empty, the function will block until an element can be popped.
+ * Returns the popped void* element.
+ */
+void* BlockingStack_pop(BlockingStack* this);
+
+/*
+ * Returns the number of elements currently on this Stack.
+ */
+int BlockingStack_size(BlockingStack* this);
+
+/*
+ * Returns true is this Stack is empty, false otherwise.
+ */
+bool BlockingStack_isEmpty(BlockingStack* this);
+
+/*
+ * Clears this Stack returning it to an empty state.
+ */
+void BlockingStack_clear(BlockingStack* this);
+
+/*
+ * Destroys this Stack by freeing the memory used by the Stack.
+ */
+void BlockingStack_destroy(BlockingStack* this);
+
+#endif /* BLOCKING_STACK_H_ */
